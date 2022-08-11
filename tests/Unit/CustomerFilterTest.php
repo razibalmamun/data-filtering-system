79d9507c851f9e084ac415e3631a2d2ee5a13dd8
Razib Al Mamun <razib.al@gmail.com>
2022-08-11 22:43:20 +0600

    added unit test

---
+++ tests/Unit/CustomerFilterTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Tests\TestCase;
+
+class CustomerFilterTest extends TestCase
+{
+    public function testGetAllCustomers()
+    {
+        $response = $this->get('/');
+        $response->assertStatus(200);
+    }
+
+    public function testFilterByYear()
+    {
+        $response = $this->get('/filter?year=1928');
+        $response->assertStatus(200);
+    }
+
+    public function testFilterByMonth()
+    {
+        $response = $this->get('/filter?month=10');
+        $response->assertStatus(200);
+    }
+
+    public function testFilterByYearMonth()
+    {
+        $response = $this->get('/filter?year=1928&month=10');
+        $response->assertStatus(200);
+    }
+
+    public function testFilterPaginationByYear()
+    {
+        $response = $this->get('/filter?year=1928&page=2');
+        $response->assertStatus(200);
+    }
+
+    public function testFilterPaginationByMonth()
+    {
+        $response = $this->get('/filter?month=11&page=2');
+        $response->assertStatus(200);
+    }
+}
